e0a26da9b2e7afed622fc0b22ee58a5002aca427
Apurbo <apurbo810@gmail.com>
2026-07-21 11:55:30 +0600

    update baseline-browser-mapping and fix page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,8 @@ import Projects from "./sections/Projects"
 
 export default function Page() {
   return (
-    <main
-  className="bg-tech-pattern origin-top"
-  style={{ transform: "scale(0.9)", width: "111.111%" }}
->
+<main className="bg-tech-pattern max-w-[1600px] mx-auto">
+  
       <section id="home" className="scroll-mt-20">
         <Hero />
       </section>
